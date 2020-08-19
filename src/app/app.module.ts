e6591b851361b52349b6838d89ac4b38ced023b2
QianDashuai <491787891@qq.com>
2020-08-19 16:01:36 +0800

    [add]完成用户相关静态页面

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 //Module
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
 //Component
@@ -11,6 +12,10 @@ import { PromptMessageComponent } from './prompt-message/prompt-message.componen
 //Service 
 import { MessageService } from './service/message.service'
 import { StorageService } from './service/storage.service';
+import { HttpClientModule } from '@angular/common/http';
+import { UserService } from './service/user.service'
+import { HttpClientService } from './service/http-client.service'
+import { LoaderService } from './service/loader.service'
 
 // import module
 import { ElModule } from 'element-angular'
@@ -28,9 +33,11 @@ import 'element-angular/theme/index.css'
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
     ElModule.forRoot(),
   ],
-  providers: [MessageService,StorageService],
+  providers: [UserService,HttpClientService,LoaderService,MessageService,StorageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { UserService } from '../service/user.service'
+import { MessageService } from '../service/message.service';
+
+import {
+  Subscription,
+} from 'rxjs';
+
+import {
+  OK_RESPONSE_NUMBER,
+} from '../../app/constants'
+
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +20,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  //管控当前用户操作卡片状态:1为登录，2为注册，3为修改密码
+  moduleIndex = 1
+
+  //登录卡片相关变量
+  loginPhone = ""
+  loginPassword = ""
+
+  //注册卡片相关变量
+  registerCodeStr = ""
+  registerPhone = ""
+  registerPassword = ""
+  registerAgainPassword = ""
+
+  //找回密码相关变量
+  forgetCodeStr = ""
+  forgetPhone = ""
+  forgetPassword = ""
+  forgetAgainPassword = ""
+
+  unClickTime = 0
+  timer: any
+
+  userSubscription: Subscription
+
+  constructor(private location: Location,
+    private userService: UserService,
+    private messageService: MessageService,
+    private route: ActivatedRoute,
+    private router: Router,) { }
 
   ngOnInit() {
   }
@@ -19,11 +60,50 @@ export class LoginComponent implements OnInit {
 
   //跳转注册
   register(){
-
+    
   }
 
   //忘记密码
   forget(){
 
+  }
+
+  //切换模块
+  changeModule(index){
+    this.moduleIndex = index
+  }
+
+  //发送验证码 type为1是注册，2是修改密码
+  sendCode(type){
+    var phoneStr = ""
+    if(type == 1){
+      phoneStr = this.registerPhone
+    }else{
+      phoneStr = this.forgetPhone
+    }
+    this.userSubscription = this.userService.code(phoneStr).subscribe(response => {
+      if (response.F_responseNo == OK_RESPONSE_NUMBER) {
+        this.messageService.sendPromptMessage("获取成功")
+        this.startUnClickTime()
+      } else {
+        this.messageService.sendPromptMessage(response.F_responseMsg)
+      }
+    });
+    
+  }
+
+  //验证码倒计时
+  startUnClickTime() {
+    this.unClickTime = 60
+
+    this.timer = setInterval(() => {
+
+      this.unClickTime = this.unClickTime -1
+
+      if (this.unClickTime<=0){
+        clearInterval(this.timer)
+      }
+  
+    },1000);
   }
 }
